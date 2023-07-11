63bd14db53249b88bba17ff441c1ce3d3895fde5
tayongcarlson <tayongcarlson@gmail.com>
2023-07-11 11:24:02 +0100

    initial commit

---
+++ Introduction.js
@@ -0,0 +1,47 @@
+//This is a coment
+/*
+Thsi is a multi-line comment
+*/
+
+//console.log displays a message on screen
+
+
+/*VARIABLE DECLARATION*/
+var myvariable = "Hello World!"
+let varname = "Carlson"
+const value = 20
+
+/*The content of the Let and var variable declarations can be changed
+in the code while the valueasign to const cannot be modified
+*/
+
+//typeof is used to know the variable's data type
+
+console.log(typeof(value))
+console.log(typeof(myvariable))
+console.log(varname)
+
+console.log(value)
+console.log(myvariable)
+console.log(varname)
+
+
+
+/* OPERATORS IN JAVASCRIPT */
+//Addition, Substraction, Multiplication, Division, Modulo
+var a=2, b=10;
+console.log(a + b)
+console.log(a - b)
+console.log(a * b)
+console.log(a / b)
+console.log(a % b)
+
+
+/* INCREMENT AND DECREMENT
+ */
+var value1 = 5, value2 = 10;
+console.log(value1, value2)
+
+value1++;
+value2++;
+console.log(value1, value2)
